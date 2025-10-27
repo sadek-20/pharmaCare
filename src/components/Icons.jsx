de2667a014925702cd7f02eb21dd3b60a5c9883a
sadek <saqavic1234@gmail.com>
2025-10-27 15:17:38 +0300

    invoices

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -303,6 +303,38 @@ export const Filter = ({ className }) => (
   </svg>
 );
 
+export const Printer = ({ className = "w-6 h-6" }) => (
+  <svg
+    className={className}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+    />
+  </svg>
+);
+
+export const Save = ({ className = "w-6 h-6" }) => (
+  <svg
+    className={className}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+    />
+  </svg>
+);
+
 export const Download = ({ className }) => (
   <svg
     className={className}
